8bae2503ddb2bccaffa9fed09957c8f114548e37
fangtoby <fangtoby@live.cn>
2016-05-08 08:51:57 +0800

    aysc

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,27 @@ var gulp = require('gulp');
 var gulpLess = require('gulp-less')
 var notify = require('gulp-notify')
 var minifyCss = require('gulp-minify-css')//压缩css
+var browserSync = require('browser-sync').create()
+
+var reload   = browserSync.reload;
 
 var log = function (msg) {
     console.log(msg)
 }
+// Domain server
+
+gulp.task('default',['watch'])
+
+gulp.task('browser-sync', function() {
+   browserSync.init({
+       server: "./"
+   });
+});
+
 //less编译
-gulp.task('watch', function () {
-    return gulp.watch(['assets/css/**/*.less'], function (event) {
+gulp.task('watch',['browser-sync'], function () {
+
+    gulp.watch(['assets/css/**/*.less'], function (event) {
         return gulp
             .src(['assets/css/[^(_)]**/[^(_)]*.less'], {
                 base: './assets'
@@ -21,6 +35,7 @@ gulp.task('watch', function () {
             .pipe(minifyCss())
             .pipe(gulp.dest('./public'))
             .pipe(notify({ message: 'path->'+event.path }))
-            .pipe(notify({ message: 'Less task complete' }));
+            .pipe(notify({ message: 'Less task complete' }))
+            .pipe(reload({stream: true}));
     });
 });
